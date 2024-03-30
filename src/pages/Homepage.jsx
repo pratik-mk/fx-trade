0b1506f57997721c1cf8eb18487f15e329081d91
Pratik Dhoke <pratikdhoke13@gmail.com>
2024-03-30 18:39:40 +0530

    Added Display card

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,6 +2,8 @@ import { Container } from "@mui/material";
 import Card from "../components/Card/Card";
 import Table from "../components/Table/Table";
 import { columns, rowData } from "../services/api";
+import TrendingDownIcon from "@mui/icons-material/TrendingDown";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
 
 const Homepage = () => {
   return (
@@ -9,7 +11,22 @@ const Homepage = () => {
       <div style={{ display: "flex", justifyContent: "space-between" }}>
         <Card title={"Total Accounts"} text={"20"} />
         <Card title={"Total EA's"} text={"20"} />
-        <Card title={"Highest Profit"} text={"$200"} />
+        <Card
+          title={"Highest Profit"}
+          text={
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              $200{" "}
+              {/* <TrendingDownIcon style={{ color: "red", fontSize: "30px" }} /> */}
+              <TrendingUpIcon style={{ color: "green", fontSize: "30px" }} />
+            </div>
+          }
+        />
       </div>
       <Table rowData={rowData} columns={columns} />
     </Container>
