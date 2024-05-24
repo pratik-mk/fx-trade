7f9ef4d53e4f1cf8f75ed5e636a54c86a5e09ecb
Pratik Dhoke <pratikdhoke13@gmail.com>
2024-05-24 18:12:56 +0530

    Added Date Selector and written API function

--- src/pages/MagicData.jsx
+++ src/pages/MagicData.jsx
@@ -1,11 +1,39 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { useParams } from "react-router-dom";
 import MagicDataTable from "../components/MagicDataTable/MagicDataTable";
-import { getAllTrades } from "../services/api";
+import { getAllTrades, getAllTradesByDate } from "../services/api";
+import { Container, TextField, Button, Typography } from "@mui/material";
 
 const MagicData = () => {
-  let { acc_no } = useParams();
+  const [dateValue, setDateValue] = useState({
+    startDate: "",
+    endDate: "",
+  });
   const [magicNumData, setMagicNumData] = useState([]);
+
+  let { acc_no } = useParams();
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    console.log(name, value);
+    setDateValue({
+      ...dateValue,
+      [name]: value,
+    });
+  };
+
+  console.log("DATEVALUE", dateValue.startDate, dateValue.endDate);
+
+  const fetchTradesByDate = useCallback(async () => {
+    console.log("API_PAYLOAD", dateValue.startDate, dateValue.endDate);
+    const response = await getAllTradesByDate(
+      acc_no,
+      dateValue.startDate,
+      dateValue.endDate
+    );
+    console.log("FETCH_TRADE_BY_DATE: ", response);
+    // setMagicNumData(response.data.data.data);
+  }, [acc_no]);
 
   const fetchAllTrades = useCallback(async () => {
     const response = await getAllTrades(acc_no);
@@ -17,9 +45,44 @@ const MagicData = () => {
   }, [fetchAllTrades]);
 
   return (
-    <div>
+    <Container maxWidth="lg">
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: "15px",
+          margin: "30px 0",
+        }}
+      >
+        <Typography variant="h5" gutterBottom>
+          Start Date
+        </Typography>
+
+        <TextField
+          id="outlined-basic"
+          name="startDate"
+          variant="outlined"
+          type="date"
+          value={dateValue.startDate}
+          onChange={handleChange}
+        />
+        <Typography variant="h5" gutterBottom>
+          End Date
+        </Typography>
+        <TextField
+          id="outlined-basic"
+          name="endDate"
+          variant="outlined"
+          type="date"
+          value={dateValue.endDate}
+          onChange={handleChange}
+        />
+        <Button variant="outlined" onClick={fetchTradesByDate}>
+          Submit
+        </Button>
+      </div>
       <MagicDataTable data={magicNumData} />
-    </div>
+    </Container>
   );
 };
 
