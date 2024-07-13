5ce09b95308deab412794f15e41188c8a6c39fee
Pratik Dhoke <pratikdhoke13@gmail.com>
2024-07-14 02:58:48 +0530

    Date Range picker behaviour and style added

--- src/components/DateRangePicker/DateRangePicker.jsx
+++ src/components/DateRangePicker/DateRangePicker.jsx
@@ -7,9 +7,38 @@ import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import "./DateRangePicker.css";
 import { Box } from "@mui/material";
 
+const DATE_SIZE = 32;
+const DATE_SIZE_STYLE = {
+  "& > div": {
+    minWidth: 256,
+  },
+  "& > div > div, & > div > div > div, & .MuiCalendarPicker-root": {
+    width: 256,
+  },
+  "& .MuiTypography-caption": {
+    width: DATE_SIZE,
+    margin: 0,
+  },
+  "& .PrivatePickersSlideTransition-root": {
+    minHeight: DATE_SIZE * 6,
+  },
+  '& .PrivatePickersSlideTransition-root [role="row"]': {
+    margin: 0,
+  },
+  "& .MuiPickersDay-dayWithMargin": {
+    margin: 0,
+  },
+  "& .MuiPickersDay-root": {
+    width: DATE_SIZE,
+    height: DATE_SIZE,
+  },
+};
+
 const DateRangePicker = () => {
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
+  const [showStartPicker, setShowStartPicker] = useState(true);
+  const [showEndPicker, setShowEndPicker] = useState(false);
   const [showPopup, setShowPopup] = useState(false);
   const popupRef = useRef(null);
 
@@ -26,58 +55,85 @@ const DateRangePicker = () => {
     };
   }, []);
 
-  // eslint-disable-next-line
   const handleStartDateChange = (date) => {
     setStartDate(date);
+    setShowStartPicker(false);
+    setShowEndPicker(true);
   };
 
   const handleEndDateChange = (date) => {
     setEndDate(date);
+    // setShowEndPicker(false);
   };
 
   const handleSubmit = () => {
     console.log("Start Date:", startDate);
     console.log("End Date:", endDate);
-    setShowPopup(false);
   };
 
   return (
     <div className="dateRangePickerContainer" ref={popupRef}>
-      <Button
-        variant="outlined"
-        color="primary"
-        onClick={() => setShowPopup(!showPopup)}
-        className="dateRangePickerButton"
-      >
-        <CalendarMonthIcon />
-        Select Dates
-      </Button>
-      {showPopup && (
-        <div className="popUp">
-          <div className="popUpItem">
-            <LocalizationProvider dateAdapter={AdapterMoment}>
-              <StaticDatePicker
-                label="End Date"
-                value={endDate}
-                onChange={handleEndDateChange}
-                renderInput={(params) => <TextField {...params} fullWidth />}
+      <LocalizationProvider dateAdapter={AdapterMoment}>
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={() => setShowPopup(!showPopup)}
+          className="dateRangePickerButton"
+        >
+          <CalendarMonthIcon />
+          Select Dates
+        </Button>
+        {showPopup && (
+          <div className="popUp">
+            <div className="popUpItem">
+              {showStartPicker && (
+                <Box sx={DATE_SIZE_STYLE}>
+                  <StaticDatePicker
+                    inputProps={{ size: "small" }}
+                    value={startDate}
+                    onChange={handleStartDateChange}
+                  />
+                </Box>
+              )}
+              {showEndPicker && (
+                <Box sx={DATE_SIZE_STYLE}>
+                  <StaticDatePicker
+                    inputProps={{ size: "small" }}
+                    value={endDate}
+                    onChange={handleEndDateChange}
+                    minDate={startDate}
+                  />
+                </Box>
+              )}
+            </div>
+            <Box sx={{ display: "flex", gap: "10px", padding: "25px 0" }}>
+              <TextField
+                label="Start Date"
+                value={startDate ? startDate.toString() : ""}
+                onClick={() => {
+                  setShowStartPicker(true);
+                  setShowEndPicker(false);
+                }}
+                readOnly
               />
-            </LocalizationProvider>
+              <TextField
+                label="End Date"
+                value={endDate ? endDate.toString() : ""}
+                onClick={() => startDate && setShowEndPicker(true)}
+                readOnly
+              />
+            </Box>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleSubmit}
+              className="submitButton"
+            >
+              Submit
+            </Button>
           </div>
-          <Box sx={{ display: "flex", gap: "10px", padding: "25px 0" }}>
-            <TextField id="start-date" label="Start Date" variant="outlined" />
-            <TextField id="end-date" label="End Date" variant="outlined" />
-          </Box>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleSubmit}
-            className="submitButton"
-          >
-            Submit
-          </Button>
-        </div>
-      )}
+        )}
+      </LocalizationProvider>
     </div>
   );
 };
